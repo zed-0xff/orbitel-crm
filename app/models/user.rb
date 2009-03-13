91dffe953bcc64bb74bf9a71d8db684282870a60
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-03-13 11:05:55 +0500

    not query redundant queries if login or email is already invalid

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ActiveRecord::Base
 
   validates_presence_of     :login
   validates_length_of       :login,    :within => 3..40
-  validates_uniqueness_of   :login
   validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
+  validates_uniqueness_of   :login,    :unless => Proc.new{ |u| u.errors.on(:email) || u.errors.on(:login) }
 
   validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
   validates_length_of       :name,     :maximum => 100
 
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
+  validates_uniqueness_of   :email,    :unless => Proc.new{ |u| u.errors.on(:email) || u.errors.on(:login) }
 
   
 
