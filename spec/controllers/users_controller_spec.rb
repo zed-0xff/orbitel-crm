fa3a8e01be2812b2bbb8a6420481d01a5bc24537
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-03-13 15:36:22 +0500

    specs for disabled signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,39 +7,36 @@ include AuthenticatedTestHelper
 describe UsersController do
   fixtures :users
 
-  it 'allows signup' do
+  it 'DO NOT allows signup' do
     lambda do
       create_user
-      response.should be_redirect
-    end.should change(User, :count).by(1)
+    end.should raise_error
   end
 
-  
 
-
-  it 'requires login on signup' do
-    lambda do
-      create_user(:login => nil)
-      assigns[:user].errors.on(:login).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  it 'requires password on signup' do
-    lambda do
-      create_user(:password => nil)
-      assigns[:user].errors.on(:password).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  it 'requires password confirmation on signup' do
-    lambda do
-      create_user(:password_confirmation => nil)
-      assigns[:user].errors.on(:password_confirmation).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
+#  it 'requires login on signup' do
+#    lambda do
+#      create_user(:login => nil)
+#      assigns[:user].errors.on(:login).should_not be_nil
+#      response.should be_success
+#    end.should_not change(User, :count)
+#  end
+#  
+#  it 'requires password on signup' do
+#    lambda do
+#      create_user(:password => nil)
+#      assigns[:user].errors.on(:password).should_not be_nil
+#      response.should be_success
+#    end.should_not change(User, :count)
+#  end
+#  
+#  it 'requires password confirmation on signup' do
+#    lambda do
+#      create_user(:password_confirmation => nil)
+#      assigns[:user].errors.on(:password_confirmation).should_not be_nil
+#      response.should be_success
+#    end.should_not change(User, :count)
+#  end
 
 #  it 'requires email on signup' do
 #    lambda do
