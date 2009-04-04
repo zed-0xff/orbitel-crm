9d1ffd26b8fdda6c6835e8fea1011c32f9b06734
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-04-04 15:00:45 +0600

    addres input form with autocomplete

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,7 @@
 class TicketsController < ApplicationController
+  auto_complete_for :street, :name
+  skip_before_filter :verify_authenticity_token, :only => [:auto_complete_for_street_name]
+
   def new_request
     @ticket = ConnectionPossibilityRequest.new
   end
