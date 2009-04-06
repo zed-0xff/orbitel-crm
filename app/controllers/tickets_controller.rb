e07ea54d13788ff38aca7dd7d83c1905990e5370
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-04-06 22:21:50 +0600

    revived streets autocomplete

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,8 +1,21 @@
 class TicketsController < ApplicationController
-  auto_complete_for :street, :name
+  #auto_complete_for :street, :name
   skip_before_filter :verify_authenticity_token, :only => [:auto_complete_for_street_name]
 
   before_filter :check_type, :only => :create
+
+  def auto_complete_for_street_name
+    method = 'name'
+
+    find_options = {
+      :conditions => [ "LOWER(#{method}) LIKE ?", '%' + params[:ticket][:house][:street].downcase + '%' ],
+      :order => "#{method} ASC",
+      :limit => 10 }
+
+    @items = Street.find(:all, find_options)
+
+    render :inline => "<%= auto_complete_result @items, '#{method}' %>"
+  end
 
   def new_request
     @ticket = ConnectionPossibilityRequest.new
