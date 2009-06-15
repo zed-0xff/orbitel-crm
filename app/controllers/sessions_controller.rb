9a180e68aa3d6aa7114a2fbaee0f5dcd5224dec6
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-06-15 09:55:56 +0600

    visual effects & remove i18n for now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,7 +25,7 @@ class SessionsController < ApplicationController
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
       redirect_back_or_default('/')
-      flash[:notice] = I18n.t(:logged_in)
+      #flash[:notice] = I18n.t(:logged_in)
     else
       note_failed_signin
       @login       = params[:login]
@@ -36,14 +36,14 @@ class SessionsController < ApplicationController
 
   def destroy
     logout_killing_session!
-    flash[:notice] = I18n.t(:logged_out)
+    flash[:notice] = "Вы вышли из системы"
     redirect_back_or_default('/')
   end
 
 protected
   # Track failed login attempts
   def note_failed_signin
-    flash[:error] = I18n.t(:login_failed)
+    flash[:error] = "Вход не выполнен"
     logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
   end
 end
