9dc861cefd73b07e0b7409365b27ef510060a4a6
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-06-19 12:25:34 +0600

    global tickets history at main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+  helper :tickets
 
+  def index
+    if current_user.is_a?(Admin) || current_user.is_a?(Technician)
+      @history = TicketHistoryEntry.paginate(
+        :page     => params[:page],
+        :per_page => 20,
+        :order    => "created_at DESC"
+      )
+    end
+  end
 end
