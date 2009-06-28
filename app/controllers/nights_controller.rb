135398671fd33b0ea60ebfd1aa06ffda5ed8f38c
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-06-28 23:17:35 +0600

    nights table - correct weekends & smileys display

--- app/controllers/nights_controller.rb
+++ app/controllers/nights_controller.rb
@@ -2,5 +2,6 @@ class NightsController < ApplicationController
   def index
     @start_date = Date.civil( Date.today.year, Date.today.mon )
     @end_date   = Date.civil( Date.today.year, Date.today.mon, -1 )
+    @users      = User.all
   end
 end
