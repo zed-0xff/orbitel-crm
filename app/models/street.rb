b7d855a97136ed51cf5475fd404f419e6fd0440c
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-07-02 09:52:03 +0600

    auto fix street name + street specs

--- app/models/street.rb
+++ app/models/street.rb
@@ -5,11 +5,24 @@ class Street < ActiveRecord::Base
 
   has_many :houses
 
+  before_save :fix_name
   before_save :capitalize_name
 
   def capitalize_name
     if self.name == self.name.mb_chars.downcase.to_s
       self.name = self.name.mb_chars.capitalize.to_s
     end
+  end
+
+  def fix_name
+    self.name.strip!
+    self.name.gsub!(/^[Уу][Лл]\./,'')
+    self.name.strip!
+  end
+
+  def self.find_or_initialize_by_name name
+    name.gsub!('ул.','')
+    name.strip!
+    find_by_name(name) || Street.new(:name => name)
   end
 end
--- spec/models/street_spec.rb
+++ spec/models/street_spec.rb
@@ -27,5 +27,41 @@ describe Street do
     @street.should_not be_valid
   end
 
-  it "should capitalize street names on create"
+  it "should capitalize street names on create" do
+    @street.name = 'гоголя'
+    @street.save!
+    @street.name.should == 'Гоголя'
+  end
+
+  it "should strip 'ул.' on save" do
+    @street.name = 'ул. гоголя'
+    @street.save!
+    @street.name.should == 'Гоголя'
+  end
+
+  describe "find_or_initialize_by_name" do
+    it "should find existing street by name" do
+      @street.name = 'гоголя'
+      @street.save!
+      Street.find_or_initialize_by_name('гоголя').should == @street
+    end
+
+    it "should initialize new street with name" do
+      s = Street.find_or_initialize_by_name('пролетарская')
+      s.should be_new_record
+      s.name.should == 'пролетарская'
+    end
+
+    it "should strip 'ул.' on find" do
+      @street.name = 'гоголя'
+      @street.save!
+      Street.find_or_initialize_by_name('ул. гоголя').should == @street
+    end
+
+    it "should strip 'ул.' on initialize" do
+      s = Street.find_or_initialize_by_name('ул. пролетарская')
+      s.should be_new_record
+      s.name.should == 'пролетарская'
+    end
+  end
 end
