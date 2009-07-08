d927a3ca0bf68254901ecf3453d01466576e2bb8
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-07-08 15:51:27 +0600

    all user types can see global ticket history

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,12 @@ class WelcomeController < ApplicationController
   helper :tickets
 
   def index
-    if current_user.is_a?(Admin) || current_user.is_a?(Technician)
+#    if current_user.is_a?(Admin) || current_user.is_a?(Technician)
       @history = TicketHistoryEntry.paginate(
         :page     => params[:page],
         :per_page => 20,
         :order    => "created_at DESC"
       )
-    end
+#    end
   end
 end
