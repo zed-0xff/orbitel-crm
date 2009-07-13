04e64842862c98224f721f04d811d8b7d5c712a2
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-07-13 23:53:43 +0600

    small fix

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,6 +1,12 @@
 class Phone < ActiveRecord::Base
   belongs_to :customer
 
+  def humanize
+    # 8-909-724-7755
+    # 8 3522 44-22-55
+    # 8 495 543-33-44
+  end
+
   def self.from_array_or_string src
     numbers = []
     src.to_a.each do |ph1|
@@ -13,12 +19,12 @@ class Phone < ActiveRecord::Base
 
   def self.canonicalize number
     number = number.to_s.delete('-() ')
-    # 89097247755  8-909-724-7755
-    # 83522442255  8 3522 44-22-55
-    # 84955433344  8 495 543-33-44
+    # 89097247755
+    # 83522442255
+    # 84955433344
     number = "8#{number}" if number.size == 10 && number.first != '8'
     number[0..1] = '8' if number[0..1] == '+7'
-    if number.size != 11
+    if number.size < 11
       number = '835222'[0..(10-number.size)] + number
     end
     number.to_i
