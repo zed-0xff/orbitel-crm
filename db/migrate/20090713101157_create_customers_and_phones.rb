f2108ffa9ed929f9a470c7ed80b85fc3a59ee5ea
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-07-13 16:25:16 +0600

    created Customers and Phones

---
+++ db/migrate/20090713101157_create_customers_and_phones.rb
@@ -0,0 +1,24 @@
+class CreateCustomersAndPhones < ActiveRecord::Migration
+  def self.up
+    create_table :customers do |t|
+      t.string  :name, :null => false
+      t.string  :flat
+      t.integer :karma
+
+      t.timestamps
+    end
+
+    create_table :phones do |t|
+      t.belongs_to :customer
+      t.integer :number, :limit => 8, :null => false
+    end
+
+    add_index :phones, :number, :unique => true
+    add_index :phones, :customer_id
+  end
+
+  def self.down
+    drop_table :phones
+    drop_table :customers
+  end
+end
