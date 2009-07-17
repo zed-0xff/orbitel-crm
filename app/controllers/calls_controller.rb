934286889d2c599dcdcbcab7a3ab0677756b1440
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-07-18 00:37:22 +0600

    show only 50 last calls

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,5 +1,5 @@
 class CallsController < ApplicationController
   def index
-    @calls = Radius::Call.all :order => 'acctstarttime DESC, acctstoptime DESC'
+    @calls = Radius::Call.all :order => 'acctstarttime DESC, acctstoptime DESC', :limit => 50
   end
 end
