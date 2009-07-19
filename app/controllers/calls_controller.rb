9d583c62a8c3ba041b699b7ea0daaf55fa084d47
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-07-19 21:57:28 +0600

    optimize sql

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -4,5 +4,30 @@ class CallsController < ApplicationController
       :order => 'acctstarttime DESC, acctstoptime DESC', 
       :limit => (params[:limit] || 50)
     )
+    if @calls.any?
+      h = {}
+      @calls.each do |call|
+        n = Phone.canonicalize(call.caller)
+        h[n] ||= []
+        h[n] << call
+      end
+
+      Phone.all(
+        :conditions => {:number => h.keys},
+        :include    => {:customer => {:house => :street}}
+      ).each do |phone|
+        h[phone.number].each do |call|
+          call.customer = phone.customer
+        end
+        h.delete phone.number
+      end
+
+      # remaining unknown phones
+      h.each do |n,calls|
+        calls.each do |call|
+          call.customer = nil
+        end
+      end
+    end
   end
 end
