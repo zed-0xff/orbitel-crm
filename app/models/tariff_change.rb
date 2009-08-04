b5c39a5cd4e8159f9a3d6492a2cce642cce1b364
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-08-04 21:02:13 +0600

    show tariff change ticket type info

--- app/models/tariff_change.rb
+++ app/models/tariff_change.rb
@@ -33,6 +33,10 @@ class TariffChange < Ticket
     self.dept = Dept[:admins]
   end
 
+  def tariff= value
+    self.custom_info = value.to_s
+  end
+
   def tariff
     if self.custom_info.to_s =~ /^\d+$/
       custom_info.to_i
@@ -41,7 +45,11 @@ class TariffChange < Ticket
     end
   end
 
-  def tariff= value
-    self.custom_info = value.to_s
+  def tariff_name
+    if (t = tariff).is_a?(Numeric)
+      Tariff.find_by_id(t).try(:name) || "?? #{t} ??"
+    else
+      t
+    end
   end
 end
