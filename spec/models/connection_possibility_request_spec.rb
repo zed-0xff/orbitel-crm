b3772a479474d4bd2bd9e354e866d57a05aaae64
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-08-23 11:01:40 +0600

    fix ConnectionPossibilityRequest specs

--- spec/models/connection_possibility_request_spec.rb
+++ spec/models/connection_possibility_request_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe ConnectionPossibilityRequest do
   before(:each) do
     @valid_attributes = {
+      :created_by_id => 1
     }
   end
 
